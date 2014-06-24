7cea60c23df038aa2a55fe361fa6dc7685c0adf7
vocoderism <davide.franzino@gmail.com>
2014-06-24 09:50:12 +0200

    Update crom.mk

--- crom.mk
+++ crom.mk
@@ -1,6 +1,6 @@
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7100/full_n7100.mk)
